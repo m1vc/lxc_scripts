908a07c281060f6d6f49dd2c4fb2b73c425b30de
m1vc <synectin@gmail.com>
2020-12-15 22:17:06 +0000

    Parametrised hostname in service files

--- checkout.sh
+++ checkout.sh
@@ -3,6 +3,6 @@ git add ./scripts
 git add ./storage
 git add ./
 git commit -m 'Parametrised hostname in service files'
-git push https://synectin\@gmail.com:dia1311nina@github.com/m1vc/lxc_scripts.git
+git push https://m1vc:dia1311nina@github.com/m1vc/lxc_scripts.git
 
 #git pull -p
