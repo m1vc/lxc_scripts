c15ef90158f48b9f92a6052472def9826b7e1a9a
m1vc <synectin@gmail.com>
2020-12-17 11:32:18 +0000

    Parametrised hostname in service files

--- checkout.sh
+++ checkout.sh
@@ -6,3 +6,19 @@ git commit -m 'Parametrised hostname in service files'
 git push https://m1vc:dia1311nina@github.com/m1vc/lxc_scripts.git
 
 #git pull -p
+
+#curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_localPeerId"}' http://localhost:9933/ 
+
+#!/bin/bash
+#OPTIONS="Hello Quit"
+#select opt in $OPTIONS; do
+#    if [ "$opt" = "Quit" ]; then
+#        echo done
+#        exit
+#    elif [ "$opt" = "Hello" ]; then
+#        echo Hello World
+#    else
+#        clear
+#        echo bad option
+#    fi
+#done
--- storage/nodeid.sh
+++ storage/nodeid.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_localPeerId"}' http://localhost:9933/ > /usr/local/etc/nodeid
+curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_networkState"}' http://localhost:9933/ > /usr/local/etc/system_networkState
